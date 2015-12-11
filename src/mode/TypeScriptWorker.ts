615f9510c239f7049ce8f48c8c2599723c418c5e
David Holmes <david.geo.holmes@gmail.com>
2015-12-11 16:57:25 -0500

    TypeScriptWorker

---
+++ src/mode/TypeScriptWorker.ts
@@ -0,0 +1,65 @@
+"use strict";
+
+import {mixin} from '../lib/oop';
+import Mirror from '../worker/Mirror';
+import EditorDocument from '../EditorDocument';
+import Sender from "../lib/Sender";
+
+/**
+ * Doesn't really do much because TypeScript requires the concept of a workspace.
+ * 
+ * However, does provide some notifications to trigger further actions.
+ * @class TypeScriptWorker
+ * @extends Mirror
+ */
+export class TypeScriptWorker extends Mirror {
+
+    /**
+     * @property options
+     * @private
+     */
+    private options;
+
+    /**
+     * @class TypeScriptWorker
+     * @constructor
+     * @param sender {Sender}
+     */
+    constructor(sender: Sender) {
+        super(sender, 500);
+
+        this.setOptions();
+
+        sender.emit('initAfter');
+    }
+
+    /**
+     * @method setOptions
+     * @param [options]
+     * @return {void}
+     * @private
+     */
+    private setOptions(options?) {
+        this.options = options || {};
+    }
+
+    /**
+     * @method changeOptions
+     * @param newOptions
+     * @return {void}
+     * @private
+     */
+    private changeOptions(newOptions): void {
+        mixin(this.options, newOptions);
+        this.deferredUpdate.schedule(100);
+    }
+
+    /**
+     * @method onUpdate
+     * @return {void}
+     */
+    public onUpdate(): void {
+        // The normal behaviour here is to perform a syntax check and report annotations. 
+        this.sender.emit("compiled");
+    }
+}
